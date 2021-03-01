8b31debf46268113cc99db4c93f7d75562d105fb
Akash Kumar Singh <akashkumarsingh3@gmail.com>
2021-03-01 13:37:29 +0530

    1 redundant javascript file removed

--- Routes/insertCsvToDb.js
+++
@@ -1,6 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-// const insertionMethods = require("../CollectionUtility");
-// router.post("/insertData", (req, res, next) => {
-//     insertionMethods()
-// });
